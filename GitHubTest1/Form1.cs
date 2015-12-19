36a31c457ff3057fa598e550714f3fcd533e9489
Henryk <henrykparker@gmail.com>
2015-12-19 17:40:52 +0000

    test2

--- GitHubTest1/Form1.cs
+++ GitHubTest1/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubTest1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string hello = "hello";
+        }
     }
 }
